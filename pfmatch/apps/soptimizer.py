9d667ddd06402ffcf941ef1641c047f1cb320d06
youngsm <youngsam@stanford.edu>
2023-11-20 23:47:31 -0800

    Add a working SOptimizer to apps

--- pfmatch/apps/soptimizer.py
+++ pfmatch/apps/soptimizer.py
@@ -3,110 +3,68 @@ from time import time
 
 import torch
 import yaml
-from slar.nets import SirenVis, WeightedL2Loss
 from slar.optimizers import optimizer_factory
 from slar.utils import CSVLogger, get_device
-from torch.utils.data import DataLoader, Dataset
-
-from pfmatch.apps import ToyMC
-from pfmatch.utils import load_detector_config
-from photonlib import PhotonLib
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-
-class ToyMCDataset(Dataset):
-    """
-    ToyMC track generation in the form of torch Dataset for training Siren
-    """
-    
-    def __init__(self, cfg):
-        """Constructor
-
-        Parameters
-        ----------
-        cfg : dict
-            model configuration.
-        """
-
-        self._plib = PhotonLib.load(cfg)
-        self._toymc = ToyMC(cfg, load_detector_config(cfg), self._plib)
-        
-        this_cfg = cfg.get('data')
-        self._num_tracks = this_cfg['dataset']['size']
-        
-        # todo: vis weights like in PhotonLibDataset?
-        
-    def __len__(self):
-        return self._num_tracks
-
-    def __getitem__(self, idx):
-        """
-        Parameters
-        ----------
-        idx : int
-            index of the track to generate
-
-        Returns
-        -------
-        tuple
-            (qcluster, flash) pair
-        """
-        track = self._toymc.gen_trajectories(1)
-        qpt_v = self._toymc.make_qpoints([track])
-        pe_v, *__ = self._toymc.make_photons(qpt_v)   
-        
-        # normalize qpt coords to [-1,1]     
-        qpt_v[:,:3] = self._plib.meta.norm_coord(qpt_v[:,:3])
-        
-        output = dict(
-            qpt_v=qpt_v,
-            pe_v=pe_v,
-            # todo: weight?
-        )
-        
-        return output
-    
-class SirenTrack(SirenVis):
-    def __init__(self, cfg : dict, ckpt_file : str = None):
-        # this should work with siren's already trained with voxel data!
-        super().__init__(cfg, ckpt_file)
-
-    def forward(self, x):
-        coord, pe = x[:,:3], x[:,3]
-        
-        out = super().forward(coord)       # vis in log scale
-        out = super()._inv_xform_vis(out)  # vis in linear scale
-
-        # todo: apply poisson like in ToyMC.make_photons?
-        pe_pred = torch.sum(out*pe.unsqueeze(-1), axis=0)
-
-        return pe_pred
+from pfmatch.algorithms import PoissonMatchLoss, SirenTrack
+from pfmatch.apps.toymc import ToyMCDataset
 
 
 class SOptimizer:
-    def __init__(self, cfg:dict, plib: PhotonLib | SirenVis):
-        self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    def __init__(self, cfg:dict):
+        """Train the SirenVis model using track data.
+        
+        This class uses the ToyMCDataset to train the SirenVis model by using
+        the model to predict the photo-electron (p.e.) spectrum from a track, and then comparing
+        the predicted p.e. spectrum to the true p.e. spectrum using a Poisson loss function.
+        
+        Several classes are created using the configuration dictionary. Thus, you should include
+        the corresponding keys for each class. The following classes are created:
+        - `pfamtch.algorithms.SirenTrack`: a wrapper around `SirenVis` (cfg['model'])
+        - `pfmatch.apps.toymc.ToyMCDataset`: a dataset for the track data (cfg['data']['dataset'])
+            - if generating tracks, `photonlib.PhotonLib` is created (cfg['photonlib'])
+            - if generating tracks, `pfmatch.apps.ToyMC` is created (cfg['ToyMC'] and cfg['Detector'])
+        - `torch.io.data.DataLoader`: a dataloader for ToyMCDataset (cfg['data']['loader'])
+        - `slar.utils.CSVLogger`: a logger for the training process (cfg['logger'])
+        - `torch.optim.Optimizer`: an optimizer for the SirenVis model (cfg['train'])
+        
+        The main method is `train()`, which runs the training loop. See the example notebook
+        `Train_SOptimizer.ipynb` for an example configuration and usage.
+        """
+
         if cfg.get('device'):
             self._device = get_device(cfg['device']['type'])
+        else:
+            self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         
         # essentials
-        self._plib = plib
         self._model = SirenTrack(cfg).to(self._device)
-        self._criterion = WeightedL2Loss()
-        
+        self._criterion = PoissonMatchLoss().to(self._device)
+
         # training things
         dataset = ToyMCDataset(cfg)
-        self._dataloader = DataLoader(dataset, **cfg['data']['loader'])
-        self._opt, self.epoch = optimizer_factory(self._model.parameters(), cfg)
+        self._dataloader = DataLoader(dataset, collate_fn=dataset.collate_fn, **cfg['data']['loader'])
+        self._opt, epoch = optimizer_factory(self._model.parameters(), cfg)
         self._logger = CSVLogger(cfg)
+        
+        # resume training?
         self.iteration, self.epoch = 0, 0
+        ckpt_file = cfg['model'].get('ckpt_file')
+        if ckpt_file and cfg['train'].get('resume'):
+            import re
+            # iteration-{}-epoch-{}.ckpt
+            iteration, epoch = re.findall(r'iteration-(\d+)-epoch-(\d+).ckpt',ckpt_file)[0]
+            self.iteration, self.epoch = int(iteration), int(epoch)
+            print('[SOptimizer] resuming training from iteration',self.iteration,'epoch',self.epoch)
 
         # training hyperparameters
         train_cfg = cfg.get('train',dict())
-        self.epoch_max = train_cfg.get('max_epochs',int(1e20)),
-        self.iteration_max = train_cfg.get('max_iterations',int(1e20)),
-        self.save_every_iterations = train_cfg.get('save_every_iterations',-1),
-        self.save_every_epochs = train_cfg.get('save_every_epochs',-1)  
+        self.epoch_max = train_cfg.get('max_epochs',int(1e20))
+        self.iteration_max = train_cfg.get('max_iterations',int(1e20))
+        self.save_every_iterations = train_cfg.get('save_every_iterations',-1)
+        self.save_every_epochs = train_cfg.get('save_every_epochs',-1)
 
         # save config
         with open(os.path.join(self._logger.logdir,'train_cfg.yaml'), 'w') as f:
@@ -115,44 +73,48 @@ class SOptimizer:
 
     @property
     def model(self):
+        """SirenVis model"""
         return self._model
     
     @property
     def criterion(self):
+        """Loss function for the training process (PoissonMatchLoss)"""
         return self._criterion
         
     @property
-    def plib(self):
-        return self._plib
-    
-    @property
     def dataloader(self):
+        """torch dataloader for the ToyMC dataset"""
         return self._dataloader
     
     @property
     def logger(self):
+        """logger for the training process"""
         return self._logger
 
     @property
     def opt(self):
+        """torch optimizer (Adam)"""""
         return self._opt
         
     def step(self, input):
-        """fit the model to the input data
+        """Runs a single step of the optimizer.
 
         Parameters
         ----------
-        input : torch.Tensor
-            modified qpt_v tensor with normalized coordinates and linear pe counts.
-            it is assumed that the inputs are already shuffled and batched, and on
-            the correct device.
+        input : dict
+            a dictionary containing a batch of qpt_v, pe_v, and q_sizes, as returned by
+            `ToyMCDataset.__getitem__`.
         """
 
-        input = input['qpt_v'].to(self._device)
-        target = input['pe_v'].to(self._device)
+        input['qpt_v'] = input['qpt_v'].to(self._device)
+        input['pe_v'] = input['pe_v'].to(self._device)
+        input['q_sizes'] = input['q_sizes'].to(self._device)
 
         # run model
-        pred = self.model(input)
+        out = self.model(input)
+        target = input['pe_v']
+        pred = out['pe_v']
+
         # compute loss
         loss = self.criterion(pred, target) # todo: weight=1 for now
 
@@ -164,24 +126,28 @@ class SOptimizer:
         return target, pred, loss
     
     def train(self):
+        """
+        Trains SIREN using the ToyMC dataset.
+        """
         twait = time()
         stop_training = False
     
+        # epoch loop
         while self.iteration < self.iteration_max and \
               self.epoch < self.epoch_max:  
-
-            for data in tqdm(self.dataloader, desc='Epoch %-3d'%self.epochs):
+            # iteration loop (batch loop)
+            for batch in tqdm(self.dataloader, desc='Epoch %-3d'%self.epoch, unit='batch'):
                 self.iteration += 1
                 twait = time() - twait
 
                 # step the model                    
                 ttrain = time()
-                target, pred, loss = self.step(data)
+                target, pred, loss = self.step(batch)
                 ttrain = time() - ttrain
                 
                 # log training parameters
                 self.logger.record(['iter','epoch','loss','ttrain','twait'],
-                                    [self.iteration, self.epoch, loss.item(), loss, ttrain, twait])
+                                    [self.iteration, self.epoch, loss.item(), ttrain, twait])
                 twait = time()
 
                 # step the logger (pe spectrum)
@@ -205,14 +171,15 @@ class SOptimizer:
             if (self.save_every_epochs*self.epoch) > 0 and self.epoch % self.save_every_epochs == 0:
                 self.save(count=self.iteration/len(self.dataloader.dataset))
             
-        print('[train] Stopped training at iteration',self.iteration,'epochs',self.epoch)
+        print('[SOptimizer] Stopped training at iteration',self.iteration,'epochs',self.epoch)
         self.logger.write()
         self.logger.close()
 
 
     def save(self, count=None):
+        """Saves the model parameters to a checkpoint file."""
         if count is None:
-            count = self.iteration
+            count = self.epoch
 
         filename = os.path.join(self.logger.logdir,'iteration-%06d-epoch-%04d.ckpt')
         self.model.save_state(filename % (self.iteration, self.epoch), self.opt, count)
